a8dab71e2b052dda27669b7c81a31c621fabd0b8
FrancisVarga <github@varga-net.com>
2012-11-20 22:06:42 +0100

    initial commit

---
+++ src/Processus/Client/JsonRpc/Client.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 3/6/12
+ * Time: 10:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Client\JsonRpc;
+
+class Client extends BaseHttpClient
+{
+
+}
---
+++ tests/bootrstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hissterkiller
+ * Date: 11/20/12
+ * Time: 10:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
